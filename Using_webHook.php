49081fe112e719a97c2dc0adf5efd965cc7ca98c
mostafadeveloper <mostafadeveloper@users.noreply.github.com>
2015-08-08 10:19:59 +0430

    one line added

--- Using_webHook.php
+++ Using_webHook.php
@@ -5,6 +5,7 @@
 	then when a user type a command on your telegram bot,....then ,....
 */
 // NOTE: you can pass 'true' as the second argument to decode as array
+
 $result= json_decode(file_get_contents('php://input'), true);
 error_log(print_r($result, 1), 3, '/path/to/logfile.log');
 
